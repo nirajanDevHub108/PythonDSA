2f84bd1fb23399f2ee021a46263231d610be16ab
NirajanDevHub <160125373+nirajanDevHub108@users.noreply.github.com>
2025-04-23 11:14:22 +0530

    2nd commit

--- helloworld.py
+++ helloworld.py
@@ -1,1 +1,10 @@
 print("hello Python")
+
+#Indentation
+
+#Python uses indentation for blocks, instead of curly braces. Both tabs and spaces are supported, 
+#but the standard indentation requires standard Python code to use four spaces. For example:
+
+x=1
+if x==1:
+    print("x is 1")
