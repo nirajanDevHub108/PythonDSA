11a395e257700fc587ffd1abbc40e9ceecd00099
NirajanDevHub <160125373+nirajanDevHub108@users.noreply.github.com>
2025-07-26 10:54:30 +0530

    Refactor set union operation to use the | operator for improved readability

--- Basic Python/set.py
+++ Basic Python/set.py
@@ -78,6 +78,8 @@ The symmetric_difference() method keeps all items EXCEPT the duplicates.'''
 set1 = {"a", "b", "c"}
 set2 = {1, 2, 3}
 
-set3 = set1.union(set2)
+#set3 = set1.union(set2)
+set3 = set1 | set2
 print(set3)
 
+#You can use the | operator instead of the union() method, and you will get the same result.
