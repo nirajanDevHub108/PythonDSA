036e0f86b9a836b8f03326589ee6b4480e457564
NirajanDevHub <160125373+nirajanDevHub108@users.noreply.github.com>
2025-08-01 12:04:38 +0530

    Add lambda.py to demonstrate the use of lambda functions in Python

---
+++ Basic Python/lambda.py
@@ -0,0 +1,27 @@
+#Python Lambda
+#A lambda function is a small anonymous function.
+
+#A lambda function can take any number of arguments, but can only have one expression.
+
+#syntax lambda arguments : expression
+
+#Add 10 to argument a, and return the result:
+x = lambda a : a + 10
+print(x(6))
+
+#Lambda functions can take any number of arguments:
+x = lambda a, b : a * b
+print(x(5, 6))
+
+'''The power of lambda is better shown when you use them as an anonymous function inside another function.
+
+Say you have a function definition that takes one argument, and that argument will be multiplied with an unknown number:'''
+
+def myfunc(n):
+  return lambda a : a * n
+
+mydoubler = myfunc(2)
+mytripler = myfunc(3)
+
+print(mydoubler(11))
+print(mytripler(11))
