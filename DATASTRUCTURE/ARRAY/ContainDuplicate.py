d9dc5ef5e888b0792be2c5b4fb3f547120485a0b
NirajanDevHub <160125373+nirajanDevHub108@users.noreply.github.com>
2025-12-09 23:42:34 +0530

    added an array retruning duplicate list of array elemnt

--- DATASTRUCTURE/ARRAY/ContainDuplicate.py
+++ DATASTRUCTURE/ARRAY/ContainDuplicate.py
@@ -46,3 +46,13 @@ Space complexity:
 O(n)
 O(n)
 '''
+
+# def findDuplicates(self, arr):
+#     seen=set()
+#     duplicates=set()
+#     for num in arr:
+#         if num in seen:
+#             duplicates.add(num)
+#         else:
+#             seen.add(num)
+#     return list(duplicates)
