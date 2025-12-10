c3a1ab5ab447299909c6581ad16f5fc0987c4f74
NirajanDevHub <160125373+nirajanDevHub108@users.noreply.github.com>
2025-12-10 12:53:20 +0530

    Refactor mergeNsort to check for majority element frequency in an array

--- DATASTRUCTURE/ARRAY/mergeNsort.py
+++ DATASTRUCTURE/ARRAY/mergeNsort.py
@@ -1,13 +1,35 @@
 class Solution:
-    def mergeNsort(self, arr1, arr2):
+    def mergeNsort(self, arr):
         # code here
-        res=list(set(arr1 + arr2))
-        res.sort()
-        return res
+        # res=list(set(arr1 + arr2))
+        # res.sort()
+        # return res
+        #distinct adjacent array
+        m=dict()
+        n=len(arr)
+        for i in range(n):
+            if arr[i] in m:
+                m[arr[i]] +=1
+            else:
+                m[arr[i]] =1
+        #storing the max value of lement which occours more
+        max=0
+        
+        for i in range(n):
+            if max < m[arr[i]]:
+                max=m[arr[i]]
+        #prequncy of most occur elm is less than equal to
+        #(n+1)//2
+        if max > (n+1) // 2:
+            return False
+        else:
+            return True
 
-arr1=[5,7,9]
-arr2=[4,3,7,1]
+
+
+arr=[1,1,2]
+#arr2=[4,3,7,1]
 
 sol = Solution()
-result= sol.mergeNsort(arr1,arr2)
+result= sol.mergeNsort(arr)
 print(result)
