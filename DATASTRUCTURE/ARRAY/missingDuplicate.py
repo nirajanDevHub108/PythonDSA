fd5ecccaddf687ad0e76137e136aa1d0daede688
NirajanDevHub <160125373+nirajanDevHub108@users.noreply.github.com>
2025-12-10 13:21:26 +0530

    date-11-12-25 gfg POTD- finding duplicate and missing elemnts in an array

---
+++ DATASTRUCTURE/ARRAY/missingDuplicate.py
@@ -0,0 +1,24 @@
+# Given an unsorted array arr[] of size n, containing elements from the range 1 to n, it is known that one number in this range is missing, and another number occurs twice in the array, find both the duplicate number and the missing number.
+
+
+class Solution:
+    
+    def findTwoElement(self, arr):
+        n= len(arr)
+        seen=set()
+        duplicates=[]
+        for num in arr:
+            if num in seen:
+                duplicates.append(num)
+            else:
+                seen.add(num)
+        missing=[]
+        for i in range(1,n+1):
+            if i not in seen:
+                missing.append(i)
+        return duplicates+missing
+sol=Solution()
+arr=[2,3,4,2,3,5, 12]
+res= sol.findTwoElement(arr)
+print(res)
+
