3631333b27e09ab1179db85842a51ced904a4cf5
NirajanDevHub <160125373+nirajanDevHub108@users.noreply.github.com>
2025-12-13 22:28:03 +0530

    Add example usage for swapDiagonal functionality in swapDiagonal.py

--- DATASTRUCTURE/ARRAY/swapDiagonal.py
+++ DATASTRUCTURE/ARRAY/swapDiagonal.py
@@ -7,5 +7,13 @@ class Solution:
     def swapDiagonal(self,mat):
         n=len(mat)
         for i in range(n):
+            #
             mat[i][i],mat[i][n-1-i] = mat[i][n-1-i],mat[i][i]
         return mat
+    
+sol=Solution()
+mat =[[0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8]]
+res=sol.swapDiagonal(mat)
+print(res)
