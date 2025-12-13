30562df3c7eaa04bb03c418bd5d739ba64202c35
NirajanDevHub <160125373+nirajanDevHub108@users.noreply.github.com>
2025-12-13 22:56:51 +0530

    #2nd way of fetching row and col item in matrix

--- Basic Python/2darray.py
+++ Basic Python/2darray.py
@@ -1,6 +1,11 @@
 mat = [[1,2,3],[4,5,6]]
 # Print all elements
 
-for i in range(len(mat)):
-    for j in range(len(mat[0])):
-        print(mat[i][j])
+# for i in range(len(mat)):
+#     for j in range(len(mat[0])):
+#         print(mat[i][j])
+
+#2nd way of fetching row and col item in matrix
+for row in (mat):
+    for val in (row):
+        print(val)
