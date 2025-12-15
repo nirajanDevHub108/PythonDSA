443aa63e9fdd3e74881577b5a7fdd8dfdb97f5ec
NirajanDevHub <160125373+nirajanDevHub108@users.noreply.github.com>
2025-12-15 17:11:42 +0530

    count ways using brute force

---
+++ DATASTRUCTURE/ARRAY/cntWays.py
@@ -0,0 +1,23 @@
+class Solution:
+    def cntWays(self, arr):
+        count=0
+        for i in range(len(arr)):
+            res=[]
+            for j in range(len(arr)):
+                if j != i:
+                    res.append(arr[j])
+            even_sum=0
+            odd_sum=0
+            for k in range(len(res)):
+                if k%2==0:
+                 even_sum+=res[k]   
+                else:
+                  odd_sum+=res[k]
+            if even_sum == odd_sum:
+              count+=1
+        return count
+
+sol=Solution()
+arr=[2, 1, 6, 4]
+res=sol.cntWays(arr)
+print(res)
