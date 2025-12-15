81f6e97fa1edae2515f7df16e7685216ec6f72db
NirajanDevHub <160125373+nirajanDevHub108@users.noreply.github.com>
2025-12-15 18:04:29 +0530

    optimal approach using prefix sum

--- DATASTRUCTURE/ARRAY/cntWays.py
+++ DATASTRUCTURE/ARRAY/cntWays.py
@@ -1,5 +1,6 @@
 #brute force approach using 3 for loop
-'''        for i in range(len(arr)):
+'''    count=0    
+    for i in range(len(arr)):
             res=[]
             for j in range(len(arr)):
                 if j != i:
@@ -17,9 +18,34 @@
 
 class Solution:
     def cntWays(self, arr):
+        total_even=0
+        total_odd=0
+        for i in range(len(arr)):
+            if i%2==0:
+                total_even+=arr[i]
+            else:
+                total_odd+= arr[i]
+    
         count=0
+        left_even=0
+        left_odd=0
+        # Step 2: Try removing each index
+        for i in range(len(arr)):
+            if i %2 == 0:
+                total_even-=arr[i]
+            else:
+                total_odd-=arr[i]
 
+            if left_even+total_odd == left_odd+total_even:
+                count+=1
+            
+            if i%2 == 0:
+                left_even+=arr[i]
+            else:
+                left_odd+=arr[i]
         return count
+        
+
 
 sol=Solution()
 arr=[2, 1, 6, 4]
