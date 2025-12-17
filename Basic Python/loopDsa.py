6850dc70a0494e45582f7df9f71a2147677a2886
NirajanDevHub <160125373+nirajanDevHub108@users.noreply.github.com>
2025-12-17 11:40:27 +0530

    feat added for each loop

--- Basic Python/loopDsa.py
+++ Basic Python/loopDsa.py
@@ -5,3 +5,10 @@ for i in range(len(arr)):#iterating 0 to n-1 0,1,2,3,4=5 elemnts
     if max_val < arr[i]:
         max_val=arr[i]
 print(max_val)
+
+#for each loop is useful when index is not requried like freq count,sum,validation
+
+total=0
+for num in arr:
+    total+=num
+print("total of al number is :",total)
