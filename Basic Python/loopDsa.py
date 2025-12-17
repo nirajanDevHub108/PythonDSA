0d49fc0d5786933c82678fe4f12b64918935e61c
NirajanDevHub <160125373+nirajanDevHub108@users.noreply.github.com>
2025-12-17 13:13:20 +0530

    #loop with break satatement

--- Basic Python/loopDsa.py
+++ Basic Python/loopDsa.py
@@ -31,3 +31,11 @@ print("----------------------------")
 for i in range(len(arr)):
     for j in range(i+1,len(arr)):
         print(arr[i],arr[j])
+print("----------------------------")
+
+#loop with break satatement
+target=2
+for num in arr:
+    if num == target:
+        print(target," :found")
+        break
