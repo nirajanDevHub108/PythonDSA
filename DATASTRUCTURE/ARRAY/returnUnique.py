ae5752311da18b5b725f3ab27b5a995eb0d3cd1e
NirajanDevHub <160125373+nirajanDevHub108@users.noreply.github.com>
2025-12-17 11:29:10 +0530

    feat: implement unique element with bit operation

--- DATASTRUCTURE/ARRAY/returnUnique.py
+++ DATASTRUCTURE/ARRAY/returnUnique.py
@@ -1,11 +1,24 @@
-def returnUniqueElemnt(arr):
-    n=len(arr)
-    ans=0
-    for i in range(n):
-         #if we xor using same number it will give you 0 and 0 with diffrent number will give you other number
-         ans=arr[i]^ans
-    return ans
+def returnUniqueElemnt(arr,k):
+        n=len(arr)
+        ans=0
+        max_num=max(arr)
+        max_bit=max_num.bit_length()
+        for bit in range(max_bit):
+            count=0
+            for num in arr:
+                if (num >> bit) &1:
+                    count+=1
+            if count % k != 0:
+                ans |=(1<<bit)
+          
+        return ans
+    # ans=0
+    # for i in range(n):
+    #      #if we xor using same number it will give you 0 and 0 with diffrent number will give you other number
+    #      ans=arr[i]^ans
+    # return ans
 
-arr=[2,3,4,2,3]
-res=returnUniqueElemnt(arr)
+arr=[6, 2, 5, 2, 2, 6, 6]
+k=3
+res=returnUniqueElemnt(arr,k)
 print(res)
