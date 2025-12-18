3630f26b2f1c25613424fd5a47ea46a7e443f850
NirajanDevHub <160125373+nirajanDevHub108@users.noreply.github.com>
2025-12-18 18:24:11 +0530

    added comment

--- Basic Python/reverse-array.py
+++ Basic Python/reverse-array.py
@@ -1,6 +1,6 @@
 arr=[1,2,3,4,5]
 left,right=0 ,len(arr)-1
-
+# we have implemented two pointer and reversed the array
 while left < right:
     arr[left],arr[right] = arr[right],arr[left]
     left+=1
