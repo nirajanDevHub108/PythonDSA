c175b5f0d91dbe2916f1fa33ceb26bbd2548c8ed
NirajanDevHub <160125373+nirajanDevHub108@users.noreply.github.com>
2025-12-19 14:19:49 +0530

    docs: add function description and complexity analysis for bus conductor problem

--- gfg_POTD/busConductor.py
+++ gfg_POTD/busConductor.py
@@ -1,5 +1,12 @@
 '''
+You are conductor of a bus. You are given two arrays chairs[] and passengers[] of equal length, where chairs[i] is the position of the ith chair and passengers[j] is the position of the jth passenger. You may perform the following move any number of times:
 
+Increase or decrease the position of the ith passenger by 1 (i.e., moving the ith passenger from position x to x+1 or x-1)
+Return the minimum number of moves required to move each passenger to get a chair.
+Note: Although multiple chairs can occupy the same position, each passenger must be assigned to exactly one unique chair.
+
+T(c)-O(n logn)
+space=0(1)
 '''
 class Solution:
     def findMoves(self, chairs, passengers):
