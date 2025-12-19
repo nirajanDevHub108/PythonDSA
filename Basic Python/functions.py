9ddab9d5ebd70bda34592f6b294878d88e2f7229
NirajanDevHub <160125373+nirajanDevHub108@users.noreply.github.com>
2025-12-19 14:00:33 +0530

    feat-function-arguments type

--- Basic Python/functions.py
+++ Basic Python/functions.py
@@ -1,3 +1,11 @@
 def hello_world():
     print("hello world")
 hello_world()
+
+#with argument ,parameters in function 
+def sum(num1,num2):
+    print(num1+num2)
+
+sum(2,5)
+sum(1,7)
+sum(100,200)
