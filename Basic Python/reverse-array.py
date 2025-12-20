ad214912ed51395e860e2e38d9a74353c62da9e8
NirajanDevHub <160125373+nirajanDevHub108@users.noreply.github.com>
2025-12-20 09:10:07 +0530

    feat: update reverse array implementation to handle larger arrays

--- Basic Python/reverse-array.py
+++ Basic Python/reverse-array.py
@@ -1,8 +1,17 @@
-arr=[1,2,3,4,5]
-left,right=0 ,len(arr)-1
-# we have implemented two pointer and reversed the array
-while left < right:
-    arr[left],arr[right] = arr[right],arr[left]
+arr=[1,2,3,4,5,6]
+left=0
+right=(len(arr)//2)-1
+while left <right:
+    arr[left],arr[right]=arr[right],arr[left]
     left+=1
     right-=1
 print(arr)
+
+
+# left,right=0 ,len(arr)-1
+# # we have implemented two pointer and reversed the array
+# while left < right:
+#     arr[left],arr[right] = arr[right],arr[left]
+#     left+=1
+#     right-=1
+# print(arr)
