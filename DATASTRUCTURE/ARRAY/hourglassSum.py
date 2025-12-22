f3d4b242356ff41d706270cbad5f999959f5f321
NirajanDevHub <160125373+nirajanDevHub108@users.noreply.github.com>
2025-12-22 13:24:25 +0530

    feat: implement hourglassSum function to calculate maximum hourglass sum in a 2D array

--- DATASTRUCTURE/ARRAY/hourglassSum.py
+++ DATASTRUCTURE/ARRAY/hourglassSum.py
@@ -28,3 +28,13 @@ The highest hourglass sum is  from the hourglass beginning at row , column :
 
 maxsum=28
 '''
+def hourglassSum(arr):
+    # Write your code here
+    max_sum = float('-inf')  
+    for i in range(4):
+        for j in range(4):
+            hourglass_sum=(arr[i][j]+arr[i][j+1]+arr[i][j+2]+
+                            arr[i+1][j+1]+
+                            arr[i+2][j]+arr[i+2][j+1]+arr[i+2][j+2])
+            max_sum=max(max_sum,hourglass_sum)
+    return max_sum
