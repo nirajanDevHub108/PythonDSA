119abbbe4b1415dac3b61bd6f3a99334556e00b6
NirajanDevHub <160125373+nirajanDevHub108@users.noreply.github.com>
2025-12-22 16:00:11 +0530

    docs: add docstring to getSecondLargest function explaining its purpose and usage

--- DATASTRUCTURE/ARRAY/second-largets.py
+++ DATASTRUCTURE/ARRAY/second-largets.py
@@ -1,3 +1,12 @@
+'''Given an array of positive integers arr[], return the second largest element from the array. If the second largest element doesn't exist then return -1.
+
+Note: The second largest element should not be equal to the largest element.
+
+Examples:
+
+Input: arr[] = [12, 35, 1, 10, 34, 1]
+Output: 34
+Explanation: The largest element of the array is 35 and the second largest element is 34.'''
 def getSecondLargest( arr):
         # Code Here
         second_larget=-1
