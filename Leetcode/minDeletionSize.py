9161917e684381065ffa28fd8765fc4f95cf73d9
NirajanDevHub <160125373+nirajanDevHub108@users.noreply.github.com>
2025-12-22 16:49:43 +0530

    problem-960-delete columns to make sorted II

--- Leetcode/minDeletionSize.py
+++ Leetcode/minDeletionSize.py
@@ -1,3 +1,24 @@
+'''
+problem-960-delete columns to make sorted II
+
+You are given an array of n strings strs, all of the same length.
+
+We may choose any deletion indices, and we delete all the characters in those indices for each string.
+
+For example, if we have strs = ["abcdef","uvwxyz"] and deletion indices {0, 2, 3}, then the final array after deletions is ["bef", "vyz"].
+
+Suppose we chose a set of deletion indices answer such that after deletions, the final array has every string (row) in lexicographic order. (i.e., (strs[0][0] <= strs[0][1] <= ... <= strs[0][strs[0].length - 1]), and (strs[1][0] <= strs[1][1] <= ... <= strs[1][strs[1].length - 1]), and so on). Return the minimum possible value of answer.length.
+
+ 
+
+Example 1:
+
+Input: strs = ["babca","bbazb"]
+Output: 3
+Explanation: After deleting columns 0, 1, and 4, the final array is strs = ["bc", "az"].
+Both these rows are individually in lexicographic order (ie. strs[0][0] <= strs[0][1] and strs[1][0] <= strs[1][1]).
+Note that strs[0] > strs[1] - the array strs is not necessarily in lexicographic order.'''
+
 def minDeletionSize(strs):
         """
         :type strs: List[str]
