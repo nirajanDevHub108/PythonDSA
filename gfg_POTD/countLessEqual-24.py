2136c6dcc9f3e18a4f0408d1353a8469565e6184
NirajanDevHub <160125373+nirajanDevHub108@users.noreply.github.com>
2025-12-24 15:58:50 +0530

    added comment on bruteorce code

--- gfg_POTD/countLessEqual-24.py
+++ gfg_POTD/countLessEqual-24.py
@@ -1,9 +1,13 @@
-def countLessEqual(arr, x):
+'''      Brute force solution is having O(n) Tc we need to optimize it using binary search implemented array i O(logn) Tc 
         count=0
         for num in arr:
             if num <= x:
                 count+=1
         return count
+
+        '''
+def countLessEqual(arr, x):
+
 
 arr=[6, 10, 12, 15, 2, 4, 5]
 x = 14
