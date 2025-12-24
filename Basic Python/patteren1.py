ede98d17588e98790ced3fc2b690538b2ecadcd1
NirajanDevHub <160125373+nirajanDevHub108@users.noreply.github.com>
2025-12-24 23:50:20 +0530

    pattern-4 striver logic building day 1

--- Basic Python/patteren1.py
+++ Basic Python/patteren1.py
@@ -11,6 +11,18 @@ for i in range(1,n+1):
     for j in range(1,i+1):
         print(i,end="")
     print()
+
+#1
+#22
+#333
+#4444
+#55555  
+#no of line =n
+
+for i in range(1,n+1):
+    for j in range(i):
+        print(i,end="")
+    print()
       
 
 
