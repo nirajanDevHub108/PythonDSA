a66f4929659f6656f421b4bfb0b0722ab72f1ac0
NirajanDevHub <160125373+nirajanDevHub108@users.noreply.github.com>
2025-12-25 18:37:29 +0530

    -hackerrank 2d problem diagonal-absolute sum

--- Basic Python/2darray.py
+++ Basic Python/2darray.py
@@ -9,3 +9,20 @@ mat = [[1,2,3],[4,5,6]]
 for row in (mat):
     for val in (row):
         print(val)
+
+mat = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [9, 8, 9]
+]
+left_diagonal=[]
+right_diagonal=[]
+n=len(mat)
+for i in range(len(mat)):
+        left_diagonal.append(mat[i][i])
+        right_diagonal.append(mat[i][n-i-1])
+        
+abs_diff=abs(sum(left_diagonal)-sum(right_diagonal))
+print(abs_diff)
+
+
