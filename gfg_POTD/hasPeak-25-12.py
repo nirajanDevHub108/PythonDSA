df7629a0dff7dac90f8cfc884c0e6488869f60d8
NirajanDevHub <160125373+nirajanDevHub108@users.noreply.github.com>
2025-12-25 13:16:26 +0530

    POTD-GFG-optimal solution using binary searach in O(nlog m)

--- gfg_POTD/hasPeak-25-12.py
+++ gfg_POTD/hasPeak-25-12.py
@@ -1,4 +1,4 @@
-def findPeakGrid( mat):
+'''
     n = len(mat)
     m = len(mat[0])
 
@@ -17,10 +17,41 @@ def findPeakGrid( mat):
 
             return [i, j]  
 
+    return [] T(c)=O(n*m)
+optimal will be in O(nlog m)
+using binary search
+'''
+def findPeakGrid( mat):
+    n=len(mat)#row
+    m=len(mat[0]) #column 
+
+    low=0
+    high=m-1
+    while low <=high:
+        mid=(low+high)//2
+
+        #finiding max element in mid col
+
+        max_row=0
+        for i in range(n):
+            if mat[i][mid] > mat[max_row][mid]:
+                max_row=i
+        mid_val=mat[max_row][mid]
+        left=mat[max_row][mid-1] if mid-1 >=0 else float("-inf")
+        right=mat[max_row][mid+1] if mid+1 < m else float("-inf")
+
+        if mid_val >= left and mid_val>=right:
+            return [max_row,mid]
+        elif right >mid_val:
+            low=mid+1
+        else:
+            high=mid-1
     return []
 
-mat=[[10, 20, 15],           
-    [21, 30, 14],
-    [7, 16, 32]]
+
+
+
+mat=[[46,46]]
+
 res=findPeakGrid(mat)
 print(res)
