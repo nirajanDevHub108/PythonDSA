41d64ec49a2b2f2d2f6925b4dc84182e55526bdb
NirajanDevHub <160125373+nirajanDevHub108@users.noreply.github.com>
2025-12-25 11:02:10 +0530

    pattern-9-logic builidng day 2

--- Basic Python/patteren1.py
+++ Basic Python/patteren1.py
@@ -117,6 +117,24 @@ for i in range(n):
     # for j in range(i):
     #     print(" ",end="")
     print()
+print("--------------------------")
+
+''''''
+for i in range(n):
+    #space printing
+    for j in range(n-i-1):
+        print(" ",end="")
+    
+    for j in range(2*i+1):
+        print("*",end="")
+    print()
+for i in range(n):
+    for j in range(i):
+        print(" ",end="")
+    for j in range(2*n - (2*i+1)):
+        print("*",end="")
+
+    print()
 # 1
 # 3 5
 # 7 9 11
