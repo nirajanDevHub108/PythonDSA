4ee6a881b969b4ff0649feafdd3360e102114da7
NirajanDevHub <160125373+nirajanDevHub108@users.noreply.github.com>
2025-12-26 18:07:50 +0530

    kth missing problem statement

--- SearchingAlgo/kthSmallestPosmissNum.py
+++ SearchingAlgo/kthSmallestPosmissNum.py
@@ -1,3 +1,13 @@
+'''
+Given a sorted array of distinct positive integers arr[], You need to find the kth positive number that is missing from the arr[].
+
+Examples:
+
+Input: arr[] = [2, 3, 4, 7, 11], k = 5
+Output: 9
+Explanation: Missing are 1, 5, 6, 8, 9, 10… and 5th missing number is 9.
+'''
+
 class Solution:
     def kthMissing(self, arr, k):
         low, high = 0, len(arr) - 1
