edba8ba0df403edc86bfb42b258e599b1e552076
NirajanDevHub <160125373+nirajanDevHub108@users.noreply.github.com>
2025-12-28 11:28:54 +0530

    countNegatives.potd-optimal approachPOTD-27-12-25

--- Leetcode/count-negin2dmat.py
+++ Leetcode/count-negin2dmat.py
@@ -4,11 +4,23 @@ def countNegatives(grid):
         :rtype: int
         """
         count=0
-        for row in grid:
-            for val in row:
-                if val < 0:
-                    count+=1
+        # for row in grid:
+        #     for val in row:
+        #         if val < 0:
+        #             count+=1
+        # return count
+        n=len(grid)
+        m=len(grid[0])
+        i,j=0,n-1
+        while i <m and j >=0:
+                if grid[i][j] <0:
+                    count +=m-i
+                    j-=1
+                else:
+                       i+=1
         return count
+                    
+                    
 
 grid = [[4,3,2,-1],[3,2,1,-1],[1,1,-1,-2],[-1,-1,-2,-3]]
 res=countNegatives(grid)
