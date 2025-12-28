1f83341a09b9a5f79b86bba7088e3432d666eda1
NirajanDevHub <160125373+nirajanDevHub108@users.noreply.github.com>
2025-12-28 11:37:26 +0530

    2d-matrix declearation

--- Basic Python/2darray.py
+++ Basic Python/2darray.py
@@ -6,14 +6,17 @@ mat = [[1,2,3],[4,5,6]]
 #         print(mat[i][j])
 
 #2nd way of fetching row and col item in matrix
+count=0
 for row in (mat):
     for val in (row):
-        print(val)
+        if val >0:
+             count+=1
+print(count)
 
 mat = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [9, 8, 9]
+    [1, 2, -2],
+    [4, -5, 6],
+    [9, -8, -9]
 ]
 left_diagonal=[]
 right_diagonal=[]
@@ -25,4 +28,22 @@ for i in range(len(mat)):
 abs_diff=abs(sum(left_diagonal)-sum(right_diagonal))
 print(abs_diff)
 
+#Declaration of a Matrix or two-dimensional array
+rows=3
+cols=3
+row,cols = (3,3)
+arr=[[0]*cols]*rows
+print(arr)
 
+#initialize a matrix in different languages
+# Initializing a 2-D array with values
+arr = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
+# We can perform a variety of operations on the Matrix Data Structure. Some of the most common operations are:
+
+# Access elements of Matrix
+# Traversal of a Matrix
+# Searching in a Matrix
+# Sorting a Matrix
+
+#1Access elements of Matrix Data Structure:
+'''Like one-dimensional arrays, matrices can be accessed randomly by using their indices to access the individual elements. A cell has two indices, one for its row number, and the other for its column number. We can use arr[i][j] to access the element which is at the ith row and jth column of the matrix.'''
