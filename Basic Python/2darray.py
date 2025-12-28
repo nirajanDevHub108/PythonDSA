5065bcdffeaf3d10dd86579847c996c1eb288352
NirajanDevHub <160125373+nirajanDevHub108@users.noreply.github.com>
2025-12-28 11:50:54 +0530

    sorting row wise in 2d matrix

--- Basic Python/2darray.py
+++ Basic Python/2darray.py
@@ -92,3 +92,25 @@ if search_in_mat(arr2,x):
      print("YES")
 else:
      print("NO")
+
+
+#sorting matrix row wise
+def sortRows(mat):
+    for row in mat:
+        row.sort()
+
+mat = [
+    [77, 11, 22, 3],
+    [11, 89, 1, 12],
+    [32, 11, 56, 7],
+    [11, 22, 44, 33]
+]
+
+sortRows(mat)
+
+print('[\n', end='')
+for row in mat:
+    print('  [', end='')
+    print(', '.join(map(str, row)), end='')
+    print(']')
+print(']')
