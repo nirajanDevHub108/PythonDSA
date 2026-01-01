c7fea9932cb96f7f163c802a14b7c274c6660005
NirajanDevHub <160125373+nirajanDevHub108@users.noreply.github.com>
2026-01-01 19:31:41 +0530

    recursion practice

--- Basic Python/recursion.py
+++ Basic Python/recursion.py
@@ -1,9 +1,15 @@
-#recursion fuction
-def add_one(num):
-    if (num >= 9):
-        return num+1
-    total=num+1
-    print(total)
+# #recursion fuction
+# def add_one(num):
+#     if (num >= 9):
+#         return num+1
+#     total=num+1
+#     print(total)
 
-    return add_one(total)
-add_one(0)
+#     return add_one(total)
+# add_one(0)
+
+def  sum(n):
+    if n ==1 : return 1 #base condition
+    return n+sum(n-1)
+n=5
+print(sum(n))
