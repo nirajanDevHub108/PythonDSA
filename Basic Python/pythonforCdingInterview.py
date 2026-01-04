a18eb7ea6ad0e2ccd6ca572fdb30b34b76696327
NirajanDevHub <160125373+nirajanDevHub108@users.noreply.github.com>
2026-01-04 12:27:41 +0530

    added loops and decimal division tips

--- Basic Python/pythonforCdingInterview.py
+++ Basic Python/pythonforCdingInterview.py
@@ -37,3 +37,27 @@ else:
 n,m = 1,2
 if ((n > 2 and n != m) or n == m):
     n+=1
+
+#while loops are similiar 
+# n=0
+# while n < 5:
+#     print(n)
+#     n += 1
+
+
+# for loop i = 0 to i =4
+for i in range(5):
+    print(i)
+# for loop i = 2 to i =6
+for i in range(2,6):
+    print(i)
+
+print('_________________')
+#division is decimal by default
+print(5/2)
+
+print(5 // 2) # double slash round down the decimal part
+
+print( -3 // 2)#most langauge round towards 0 by default so neg numbers will round down
+
+print(int(-3 / 2)) #workaround for rounding towards zero is to use decimal division and then convert to int
