9e937964ab139bbd9620b0e2a9d8dd56a0ea242f
NirajanDevHub <160125373+nirajanDevHub108@users.noreply.github.com>
2026-01-07 16:45:03 +0530

    description of problem

--- gfg_POTD/countDistinct-jan-7.py
+++ gfg_POTD/countDistinct-jan-7.py
@@ -1,3 +1,17 @@
+'''
+Given an integer array arr[] and a number k. Find the count of distinct elements in every window of size k in the array.
+
+Examples:
+
+Input: arr[] = [1, 2, 1, 3, 4, 2, 3], k = 4
+Output: [3, 4, 4, 3]
+Explanation:
+First window is [1, 2, 1, 3], count of distinct numbers is 3.
+Second window is [2, 1, 3, 4] count of distinct numbers is 4.
+Third window is [1, 3, 4, 2] count of distinct numbers is 4.
+Fourth window is [3, 4, 2, 3] count of distinct numbers is 3.
+'''
+
 from collections import defaultdict
 def countDistinct(arr,k):
   
