ee7d3e8297521e98081b2072f24f0cd1d2a6063a
NirajanDevHub <160125373+nirajanDevHub108@users.noreply.github.com>
2026-01-09 18:24:14 +0530

    dp problem pOTD-08-01-26

---
+++ DP/maxDotProduct.py
@@ -0,0 +1,19 @@
+class Solution:
+    def maxDotProduct(self, nums1, nums2):
+        n, m = len(nums1), len(nums2)
+        NEG_INF = -10**18
+
+        dp = [[NEG_INF] * (m + 1) for _ in range(n + 1)]
+
+        for i in range(n - 1, -1, -1):
+            for j in range(m - 1, -1, -1):
+                take = nums1[i] * nums2[j] + max(dp[i + 1][j + 1], 0)
+                skip1 = dp[i + 1][j]
+                skip2 = dp[i][j + 1]
+                dp[i][j] = max(take, skip1, skip2)
+
+        return dp[0][0]
+nums1 = [2,1,-2,5]
+nums2 = [3,0,-6]
+sol=Solution()
+print(sol.maxDotProduct(nums1,nums2))
