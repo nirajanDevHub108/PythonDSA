bce29baf5b5bb5b1b8ea46507b0a34f52b2f4a73
NirajanDevHub <160125373+nirajanDevHub108@users.noreply.github.com>
2026-01-18 16:54:45 +0530

    java code

--- STACK/nextFreqGreater.py
+++ STACK/nextFreqGreater.py
@@ -17,3 +17,53 @@ def nextFreqGreater(arr):
 arr = [2, 1, 1, 3, 2, 1]
 result = nextFreqGreater(arr)
 print(result)
+
+'''
+java code 
+
+import java.util.Stack;
+import java.util.HashMap;
+import java.util.Arrays;
+import java.util.Map;
+import java.util.ArrayList;
+public class Main
+{
+        public static ArrayList<Integer> nextGreaterFreq(int[] arr){
+            int n = arr.length;
+            Map<Integer,Integer> freq= new HashMap<>();
+            for (int num : arr){
+                freq.put(num,freq.getOrDefault(num,0)+1);
+            }
+            int [] res = new int[n];
+            Arrays.fill(res,-1);
+            Stack<Integer> st = new Stack<>();
+            
+            for (int i =0;i < n; i++){
+                
+                while (!st.isEmpty() && freq.get(arr[i]) >
+                         freq.get(arr[st.peek()])){
+                             res[st.pop()] = arr[i];
+                         }
+                st.push(i);
+            }
+            
+            ArrayList<Integer> result = new ArrayList<>();
+            for ( int x : res){
+                result.add(x);
+            }
+            return result;
+            
+            
+            
+        }
+	public static void main(String[] args) {
+	    int[] arr = {2, 1, 1, 3, 2, 1};
+	    
+		ArrayList<Integer> result = nextGreaterFreq(arr);
+		for ( int x : result){
+		    System.out.print(x + " ");
+		}
+	}
+}
+
+'''
