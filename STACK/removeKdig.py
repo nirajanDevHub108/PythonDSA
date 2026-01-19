f8f58378ea59151ca092683b7254d3dcd7d33280
NirajanDevHub <160125373+nirajanDevHub108@users.noreply.github.com>
2026-01-19 22:12:07 +0530

    java code

--- STACK/removeKdig.py
+++ STACK/removeKdig.py
@@ -23,5 +23,52 @@ result = removeKdig(s,k)
 print(result)
 
 '''
-java code
+java code:
+
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+import java.util.Stack;
+public class Main
+{
+    static  String removekdig(String s, int k){
+        int n = s.length();
+        Stack<Character> st = new Stack<>();
+        for(int i =0; i<s.length();++i){
+            char c = s.charAt(i);
+            
+            while(!st.isEmpty() && k > 0 && st.peek() > c){
+                st.pop();
+                k-=1;
+            }
+            if(!st.isEmpty() || c !='0'){
+                st.push(c);
+            }
+        }
+        while(!st.isEmpty() && k-- > 0){
+            st.pop();
+        }
+        if(st.isEmpty())
+            return "0";
+        StringBuilder res=new StringBuilder();
+        while(!st.isEmpty()){
+            res.append(st.pop());
+        }
+        return res.reverse().toString();
+        
+    }
+    
+	public static void main(String[] args) {
+	    String s = "765028321";
+        int k = 5;
+        System.out.println(removekdig(s, k));
+		
+	}
+}
+
 '''
