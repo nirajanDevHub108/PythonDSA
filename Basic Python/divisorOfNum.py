ce28c02cfd91591b7d72a2848bf7eb09ff708eb4
NirajanDevHub <160125373+nirajanDevHub108@users.noreply.github.com>
2026-01-20 22:06:30 +0530

    java code pushed

--- Basic Python/divisorOfNum.py
+++ Basic Python/divisorOfNum.py
@@ -12,5 +12,24 @@ sol=Solution()
 print(sol.divisors(n))
 
 '''
+java code:
+class Solution {
+    public int[] divisors(int n) {
+        ArrayList<Integer> res= new ArrayList<>();
+        int i = 1;
+        while ( i <= n){
+            if(n % i == 0){
+                res.add(i);
+            }
+            i++;
+        }
+        int[] result = new int[res.size()];
+        for(int j =0; j< res.size(); j++){
+                result[j]=res.get(j);
+        }
+        return result;
+
+    }
+}
 
 '''
