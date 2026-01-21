d27f07d5bd98778eceee2d8a63be387c25150bb7
NirajanDevHub <160125373+nirajanDevHub108@users.noreply.github.com>
2026-01-21 18:42:20 +0530

    java code of the sum problem

--- DATASTRUCTURE/ARRAY/sumArr.py
+++ DATASTRUCTURE/ARRAY/sumArr.py
@@ -6,3 +6,16 @@ def sumOfArr(arr):
 arr=[2,4,5,6]
 print(sumOfArr(arr))
 
+'''
+java code:
+class Solution {
+  public int sum(int arr[], int n) {
+
+    int sum = 0;
+    for (int i : arr) {
+      sum += i;
+    }
+    return sum;
+  }
+}
+'''
