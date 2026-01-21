4857eb0db1b6d160936694087ce14ce4b9b92d01
NirajanDevHub <160125373+nirajanDevHub108@users.noreply.github.com>
2026-01-21 18:25:35 +0530

    java code

--- STACK/stockSpan.py
+++ STACK/stockSpan.py
@@ -18,3 +18,44 @@ def removeKdig( arr):
 arr = [10, 4, 5, 90, 120, 80]
 result = removeKdig(arr)
 print(result)
+
+'''
+java code:
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Stack;
+
+public class Main
+{
+    static ArrayList<Integer> calculateSpan(int[] arr) {
+        int n= arr.length;
+        ArrayList<Integer> span= new ArrayList<>(Collections.nCopies(n,0));
+        Stack<Integer> st = new Stack<>();
+        for (int i =0;i< n;i++){
+            while(!st.isEmpty() && arr[st.peek()] <= arr[i]){
+                st.pop();
+            }
+            if(st.isEmpty()){
+                span.set(i,(i+1));
+            }
+            else{
+                span.set(i,(i-st.peek()));
+            }
+            st.push(i);
+            
+        }
+        return span;
+        
+    }
+    
+	public static void main(String[] args) {
+	    
+	    int[] arr={10, 4, 5, 90, 120, 80};
+	    
+        System.out.println(calculateSpan(arr));
+		
+	}
+}
+
+'''
