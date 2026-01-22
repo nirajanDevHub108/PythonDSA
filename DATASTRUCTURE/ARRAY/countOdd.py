893ad5c4d5a9d087b4487706a9b90c379c6342fd
NirajanDevHub <160125373+nirajanDevHub108@users.noreply.github.com>
2026-01-22 20:27:21 +0530

    java code for countodd fucntion problem

--- DATASTRUCTURE/ARRAY/countOdd.py
+++ DATASTRUCTURE/ARRAY/countOdd.py
@@ -10,3 +10,20 @@ sol=Solution()
 n=5
 arr=[1,2,3,4,5]
 print(sol.countOdd(arr,n))
+
+'''
+java code:
+class Solution{
+    public int countOdd(int[] arr, int n) {
+        int count = 0;
+        for (int i =0; i<n;i++){
+            if ( arr[i ]% 2 != 0){
+                count += 1;
+            }
+        }
+        return count;
+       
+    }
+}
+
+'''
