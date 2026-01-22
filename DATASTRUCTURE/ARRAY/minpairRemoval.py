8902b2c643bfc7798b0185abbe542fb79c53a52b
NirajanDevHub <160125373+nirajanDevHub108@users.noreply.github.com>
2026-01-22 17:37:59 +0530

    java code jan-22

--- DATASTRUCTURE/ARRAY/minpairRemoval.py
+++ DATASTRUCTURE/ARRAY/minpairRemoval.py
@@ -26,3 +26,56 @@ nums=[5,2,3,1]
 
 print(minimumPairRemoval(nums))
 
+'''
+java code
+
+import java.util.*;
+
+class Solution {
+
+    public int minimumOperations(int[] nums) {
+        List<Integer> list = new ArrayList<>();
+        for (int n : nums) list.add(n);
+
+        int ops = 0;
+
+        while (!isNonDecreasing(list)) {
+            int minSum = Integer.MAX_VALUE;
+            int idx = 0;
+
+            // find leftmost adjacent pair with minimum sum
+            for (int i = 0; i < list.size() - 1; i++) {
+                int sum = list.get(i) + list.get(i + 1);
+                if (sum < minSum) {
+                    minSum = sum;
+                    idx = i;
+                }
+            }
+
+            // replace the pair
+            list.set(idx, minSum);
+            list.remove(idx + 1);
+            ops++;
+        }
+
+        return ops;
+    }
+
+    private boolean isNonDecreasing(List<Integer> list) {
+        for (int i = 1; i < list.size(); i++) {
+            if (list.get(i) < list.get(i - 1)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    // Optional main for testing
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        int[] nums = {5, 2, 3, 1};
+        System.out.println(s.minimumOperations(nums)); // Output: 2
+    }
+}
+
+'''
