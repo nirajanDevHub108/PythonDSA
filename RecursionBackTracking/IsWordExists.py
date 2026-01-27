347db501af4267dd35e89227494c17c5fe1a6149
NirajanDevHub <160125373+nirajanDevHub108@users.noreply.github.com>
2026-01-27 21:38:55 +0530

    java solution

--- RecursionBackTracking/IsWordExists.py
+++ RecursionBackTracking/IsWordExists.py
@@ -35,3 +35,46 @@ sol=Solution()
 mat = [['T', 'E', 'E'], ['S', 'G', 'K'], ['T', 'E', 'L']]
 word = "GEEK"
 print(sol.exist(mat,word))
+
+'''
+java solution:
+
+class Solution {
+    public boolean exist(char[][] mat, String word) {
+        int n = mat.length;
+        int m = mat[0].length;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (dfs(mat, word, i, j, 0)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private boolean dfs(char[][] mat, String word, int i, int j, int idx) {
+        if (idx == word.length()) {
+            return true;
+        }
+
+        if (i < 0 || i >= mat.length || j < 0 || j >= mat[0].length ||
+            mat[i][j] != word.charAt(idx)) {
+            return false;
+        }
+
+        char temp = mat[i][j];
+        mat[i][j] = '#'; // mark visited
+
+        boolean found = dfs(mat, word, i+1, j, idx+1) ||
+                        dfs(mat, word, i-1, j, idx+1) ||
+                        dfs(mat, word, i, j+1, idx+1) ||
+                        dfs(mat, word, i, j-1, idx+1);
+
+        mat[i][j] = temp; // backtrack
+        return found;
+    }
+}
+
+'''
