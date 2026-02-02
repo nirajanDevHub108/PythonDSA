e5a612394bdf239115c26613637c42b6766b8e47
NirajanDevHub <160125373+nirajanDevHub108@users.noreply.github.com>
2026-02-02 20:58:03 +0530

    java code

--- DATASTRUCTURE/ARRAY/largestElement.py
+++ DATASTRUCTURE/ARRAY/largestElement.py
@@ -6,3 +6,24 @@ def largestElement(nums):
         return max
 nums=[4,5,3,7,1,9]
 print(largestElement(nums))
+
+'''
+java code;
+
+class Solution {
+    public int largestElement(int[] nums) {
+        // Arrays.sort(nums);
+
+        // return nums[nums.length - 1];
+        //optimal
+        int max=nums[0];
+        for(int i =1; i<nums.length;i++){
+            if( nums[i] > max){
+                max = nums[i];
+            }
+        }
+        return max;
+    
+    }
+}
+'''
