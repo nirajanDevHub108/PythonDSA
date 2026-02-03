14b06e329ee5eed2f95d6f9c2643bc3a0d83e0ff
NirajanDevHub <160125373+nirajanDevHub108@users.noreply.github.com>
2026-02-03 22:37:43 +0530

    Add maxProfit function to calculate maximum profit from stock prices

---
+++ DATASTRUCTURE/ARRAY/maxProfitstocby sell.py
@@ -0,0 +1,12 @@
+def maxProfit(self, prices):
+        # code here
+        min_price = float('inf')
+        max_profit = 0
+
+        for price in prices:
+            if price < min_price:
+                min_price = price
+            else:
+                max_profit = max(max_profit, price - min_price)
+
+        return max_profit
